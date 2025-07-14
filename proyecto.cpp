2e6a9b71ca897b5c2ec0649a6c421e9632776636
diegocalec <calle.diego@correounivalle.edu.co>
2025-07-14 10:49:10 -0500

    cambio en la rama

--- proyecto.cpp
+++ proyecto.cpp
@@ -5,4 +5,6 @@ int main() {
 
     cout << "Nombre: Diego Fernando Calle Varela" << endl; 
     cout << "Codigo: 2419458" << endl; 
+    cout << "Materia: Metodologias de desarrollo de software" << endl; 
+
 }
